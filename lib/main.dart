e673f75defd48fe3600fd2e483aabccab94aa9f3
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2020-02-15 14:36:31 -0600

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -32,51 +32,104 @@ class MapSampleState extends State<MapSample> {
     new Marker(markerId: MarkerId('1'), position: _kGooglePlex.target)
   ];
 
-  Set<Polyline> _drawAreaPolygon = new Set.from([]);
+  Set<Polyline> _drawAreaPolyline = new Set.from([]);
+  var _mapType = MapType.hybrid;
+
+  bool _enableDraw = false;
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 2) _resetDrawArea();
+      if (index == 1) _createEvent();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Stack(children: <Widget>[
         GoogleMap(
-          mapType: MapType.hybrid,
+          mapType: _mapType,
           initialCameraPosition: _kGooglePlex,
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
           onCameraMove: _createCleanupArea,
-          polylines: _drawAreaPolygon,
+          polylines: _drawAreaPolyline,
           markers: Set<Marker>.from(mymarkers),
         ),
+        Center(
+          child: Icon(
+            Icons.pin_drop,
+            color: Colors.greenAccent,
+            size: 40,
+          ),
+        ),
         Container(
-          child: Icon(Icons.pin_drop),
-        )
+            padding: EdgeInsets.only(top: 40),
+            child: IconButton(
+                icon: Icon(
+                  Icons.switch_camera,
+                  color: Colors.greenAccent,
+                  size: 40,
+                ),
+                onPressed: _changeMapType))
       ]),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _resetDrawArea,
-        label: Text('Reset'),
-        icon: Icon(Icons.refresh),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            title: Text('Create Event'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.refresh),
+            title: Text('Reset'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightBlueAccent,
+        onTap: _onItemTapped,
       ),
     );
   }
 
   Future<void> _resetDrawArea() async {
     setState(() {
-      _points.add(_points[0]);
+      // _points.add(_points[0]);
+      _points.removeRange(0, _points.length);
     });
-    _points = [];
   }
 
   void _createCleanupArea(CameraPosition position) {
-    _points.add(position.target);
+    if (_enableDraw) {
+      _points.add(position.target);
+      setState(() {
+        _drawAreaPolyline.add(Polyline(
+          polylineId: PolylineId("1"),
+          width: 3,
+          color: Colors.amberAccent,
+          // fillColor: Color.fromRGBO(0, 66, 42, 0.3),
+          points: _points,
+        ));
+      });
+    }
+  }
+
+  void _changeMapType() {
     setState(() {
-      _drawAreaPolygon.add(Polyline(
-        polylineId: PolylineId("1"),
-        width: 3,
-        color: Colors.amber,
-        // fillColor: Color.fromRGBO(0, 66, 42, 0.3),
-        points: _points,
-      ));
+      if (_mapType == MapType.terrain)
+        _mapType = MapType.hybrid;
+      else
+        _mapType = MapType.terrain;
     });
+  }
+
+  void _createEvent() {
+    _enableDraw = !_enableDraw;
   }
 }
