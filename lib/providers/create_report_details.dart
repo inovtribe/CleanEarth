9587aa07cbdbea3e4df1db6a030bf60877790674
muedie <lasafalus@gmail.com>
2020-04-23 22:43:54 -0500

    report is saved into firestore & image uploaded

--- lib/providers/create_report_details.dart
+++ lib/providers/create_report_details.dart
@@ -1,19 +1,61 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:location/location.dart';
 import 'package:timwan/models/trash_report.dart';
+import 'package:timwan/utils/location.dart';
 import 'package:timwan/widgets/create_report_start.dart';
+import 'package:uuid/uuid.dart';
 
 class CreateReportDetails with ChangeNotifier {
   File image;
   TrashReport report;
   List<TrashTag> tags = [];
+  StorageUploadTask task;
 
   void updateImage(File _image) async {
     image = _image;
     notifyListeners();
   }
 
+  Future<String> uploadImage() async {
+    final path = 'images/${Uuid().v1()}.jpg';
+    final ref = FirebaseStorage().ref().child(path);
+    task = ref.putFile(image);
+    print('image upload started');
+
+    var url = await (await task.onComplete).ref.getDownloadURL();
+    return url.toString();
+  }
+
   void submitReport() async {
     print('create report submit started');
+    if (image == null) {
+      print('image not selected!');
+      return;
+    }
+
+    // get user location to tag the report
+    LocationData location = await getUserLocation();
+    GeoFirePoint position = GeoFirePoint(location.latitude, location.longitude);
+
+    // upload file & get download url
+    String photo_url = await uploadImage();
+    print('got image download url');
+
+    // save report into firebase
+    report = TrashReport(
+      active: false,
+      position: position,
+      photoUrl: photo_url,
+      userId: "",
+      tags: tags.map((tag) => tag.name).toList(),
+      timestamp: DateTime.now().toUtc(),
+    );
+    Firestore _firestore = Firestore.instance;
+    _firestore.collection('reports').add(report.toJson());
+    print('added report to firestore');
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -19,6 +20,12 @@ class HomeScreenState extends State<HomeScreen> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  void initState() {
+    super.initState();
+    final auth = FirebaseAuth.instance;
+    auth.signInAnonymously();
+  }
 
   @override
   Widget build(BuildContext context) {
