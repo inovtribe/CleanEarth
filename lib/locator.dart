ec39fd8ff26eca303e3b85321386f609c1a21273
muedie <lasafalus@gmail.com>
2020-04-26 20:14:17 -0500

    extracted image picker & upload logic to services

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:timwan/services/authentication_service.dart';
+import 'package:timwan/services/cloud_storage_service.dart';
 import 'package:timwan/services/firestore_service.dart';
+import 'package:timwan/services/image_picker_service.dart';
 import 'package:timwan/services/location_service.dart';
 import 'package:timwan/services/navigation_service.dart';
 
@@ -11,4 +13,6 @@ void setupLocator() {
   locator.registerLazySingleton(() => FirestoreService());
   locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => LocationService());
+  locator.registerLazySingleton(() => CloudStorageService());
+  locator.registerLazySingleton(() => ImagePickerService());
 }
---
+++ lib/services/cloud_storage_service.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
+class CloudStorageService {
+  Future uploadImage({
+    @required File image,
+    @required String uid,
+  }) async {
+    var imagePath = "images/$uid/${Uuid().v1()}.jpg";
+
+    try {
+      final StorageReference ref =
+          FirebaseStorage.instance.ref().child(imagePath);
+
+      StorageUploadTask uploadTask = ref.putFile(image);
+      StorageTaskSnapshot snapshot = await uploadTask.onComplete;
+
+      var imageUrl = await snapshot.ref.getDownloadURL();
+      if (uploadTask.isComplete) {
+        return CloudStorageResult(
+          imageUrl: imageUrl.toString(),
+          imagePath: imagePath,
+        );
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future deleteImage({
+    @required String imagePath,
+  }) async {
+    final StorageReference ref =
+        FirebaseStorage.instance.ref().child(imagePath);
+
+    try {
+      await ref.delete();
+      return true;
+    } catch (e) {
+      e.toString();
+    }
+  }
+}
+
+class CloudStorageResult {
+  final String imageUrl;
+  final String imagePath;
+
+  CloudStorageResult({this.imageUrl, this.imagePath});
+}
