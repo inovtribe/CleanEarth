16c1a7827d42b9d41620330d4d51938c570ac1ea
muedie <lasafalus@gmail.com>
2020-04-26 19:06:47 -0500

    added methods to populate dashboard

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:timwan/models/cleanup_event.dart';
+import 'package:timwan/models/reports_stats.dart';
+import 'package:timwan/models/trash_report.dart';
 import 'package:timwan/models/user.dart';
 
 class FirestoreService {
@@ -8,6 +14,19 @@ class FirestoreService {
       Firestore.instance.collection('users');
   final CollectionReference _eventsCollectionReference =
       Firestore.instance.collection('events');
+  final CollectionReference _reportscollectionReference =
+      Firestore.instance.collection('reports');
+
+  Geoflutterfire geo = Geoflutterfire();
+
+  /// key in document snapshot where location data is stored
+  String field = 'position';
+
+  final StreamController<ReportsStats> _nearbyReportsStatsController =
+      StreamController<ReportsStats>.broadcast();
+
+  final StreamController<List<CleanupEvent>> _nearbyEventsController =
+      StreamController<List<CleanupEvent>>.broadcast();
 
   Future createEvent(CleanupEvent event) async {
     try {
@@ -19,6 +38,64 @@ class FirestoreService {
       }
       return e.toString();
     }
+  }
+
+  Stream listenToNearbyEvents({
+    @required GeoFirePoint point,
+    double radius = 50.0,
+  }) {
+    try {
+      geo
+          .collection(collectionRef: _eventsCollectionReference)
+          .within(center: point, radius: radius, field: field)
+          .listen((snapshot) {
+        var events =
+            snapshot.map((document) => CleanupEvent.fromJson(document.data)).toList();
+
+        _nearbyEventsController.add(events);
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+
+    return _nearbyEventsController.stream;
+  }
+
+  Stream listenToNearbyReportsStats({
+    @required GeoFirePoint point,
+    double radius = 25.0,
+  }) {
+    try {
+      geo
+          .collection(collectionRef: _reportscollectionReference)
+          .within(
+            center: point,
+            radius: radius,
+            field: field,
+            strictMode: true,
+          )
+          .listen((snapshot) {
+        if (snapshot.isNotEmpty) {
+          var reports =
+              snapshot.map((document) => TrashReport.fromJson(document.data));
+
+          var stats = ReportsStats();
+          for (var report in reports) {
+            if (report.active) {
+              stats.needHelp += 1;
+            } else {
+              stats.cleanedUp += 1;
+            }
+          }
+
+          _nearbyReportsStatsController.add(stats);
+        }
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+
+    return _nearbyReportsStatsController.stream;
   }
 
   Future createUser(User user) async {
