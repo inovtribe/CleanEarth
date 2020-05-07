9725643daf99b0de31210449fa0fd01cc6ae2c82
muedie <lasafalus@gmail.com>
2020-05-06 23:57:33 -0500

    added markers + label where trash is reported

--- lib/ui/screens/reports_map_screen.dart
+++ lib/ui/screens/reports_map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:timwan/models/trash_report.dart';
 
 class ReportsMapScreen extends StatelessWidget {
@@ -11,17 +12,53 @@ class ReportsMapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(reports);
+    LatLng _center = LatLng(30, 40);
+    if (reports != null && reports.isNotEmpty) {
+      _center = LatLng(
+        reports.first.position.latitude,
+        reports.first.position.longitude,
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        // TODO: extend maps to top of screen
+        title: Text(
+          'Reports',
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         iconTheme: IconThemeData(
           color: Colors.black,
         ),
       ),
-      body: Container(),
+      body: GoogleMap(
+        mapType: MapType.terrain,
+        markers: _createMarkers(),
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 10,
+        ),
+      ),
     );
+  }
+
+  Set<Marker> _createMarkers() {
+    return reports
+        .map((e) => Marker(
+            markerId: MarkerId(e.hashCode.toString()),
+            infoWindow: InfoWindow(title: e.tags.toString()),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              e.cleanerUid == null || e.cleanerUid.isEmpty
+                  ? BitmapDescriptor.hueRed
+                  : BitmapDescriptor.hueGreen,
+            ),
+            position: LatLng(
+              e.position.latitude,
+              e.position.longitude,
+            )))
+        .toSet();
   }
 }
