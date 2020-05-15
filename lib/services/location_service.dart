4c1d5bfbf80e3ee1fcfdc3ea9fbb13a9fe0d5cc5
muedie <lasafalus@gmail.com>
2020-05-15 14:22:35 -0500

    added stream in location service

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:location/location.dart';
 
 class LocationService {
   GeoFirePoint _currentLocation;
   GeoFirePoint get currentLocation => _currentLocation;
+
+  StreamSubscription<LocationData> _locationStream;
 
   Location location = new Location();
 
@@ -19,6 +23,31 @@ class LocationService {
     }
 
     return _currentLocation;
+  }
+
+  Future listenToUserLocation() async {
+    try {
+      location.requestPermission().then((value) {
+        if (value == PermissionStatus.granted) {
+          _locationStream = location.onLocationChanged.listen((_locationData) {
+            if (_locationData != null) {
+              _currentLocation = GeoFirePoint(
+                _locationData.latitude,
+                _locationData.longitude,
+              );
+            }
+          });
+        }
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void cancelLocationStream() {
+    if (_locationStream != null) {
+      _locationStream.cancel();
+    }
   }
 
   double getDistance({
